beac98540533db478b33c8aa6b80cce97f4504a0
Md. Nahidul Islalm <nhwnahid@gmail.com>
2026-05-12 02:28:02 +0600

    Dev (#324)
    * fix: add husky setup for push
    
    * chore: cleanup warnings and migrate to eslint cli

---
+++ apps/client/eslint.config.mjs
@@ -0,0 +1,13 @@
+import nextConfig from "eslint-config-next";
+
+export default [
+  ...nextConfig,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/refs": "off",
+      "import/no-anonymous-default-export": "off",
+    },
+  },
+];
